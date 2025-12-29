cb966e0fb8157427551e7f0a0d9c0e6520a8a4cf
Yan Silva <yandsilva94@gmail.com>
2025-12-29 11:33:27 -0300

    feat<auth>: add sistema de validação nos campos de input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { SignIn } from "@/pages/sign-in";
+import { SignUp } from "@/pages/sign-up";
 import { Route, Routes } from "react-router-dom";
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
 }
---
+++ frontend/src/components/sign-in-form.tsx
@@ -0,0 +1,114 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.email("Email inválido"),
+  password: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
+});
+
+type SignInFormData = z.infer<typeof schema>;
+
+export function SignInForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormData>({
+    mode: "onBlur",
+    resolver: zodResolver(schema),
+  });
+
+  function onSubmit(data: any) {
+    console.log(data);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={cn("flex flex-col gap-6", className)}
+      {...props}
+    >
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">Faça login na sua conta</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Insira seu e-mail abaixo para acessar sua conta
+          </p>
+        </div>
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input
+            {...register("email")}
+            id="email"
+            type="email"
+            placeholder="y@examplo.com"
+            required
+          />
+          {errors.email && (
+            <FieldDescription className="text-red-600">
+              {errors.email.message}
+            </FieldDescription>
+          )}
+        </Field>
+        <Field>
+          <div className="flex items-center">
+            <FieldLabel htmlFor="password">Senha</FieldLabel>
+            <a
+              href="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Esqueceu sua senha?
+            </a>
+          </div>
+          <Input
+            {...register("password")}
+            id="password"
+            type="password"
+            required
+          />
+          {errors.password && (
+            <FieldDescription className="text-red-600">
+              {errors.password.message}
+            </FieldDescription>
+          )}
+        </Field>
+        <Field>
+          <Button type="submit">Login</Button>
+        </Field>
+        <FieldSeparator>Ou continue com</FieldSeparator>
+        <Field>
+          <Button variant="outline" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                fill="currentColor"
+              />
+            </svg>
+            Faça login com GitHub
+          </Button>
+          <FieldDescription className="text-center">
+            Não tem uma conta?{" "}
+            <a href="/sign-up" className="underline underline-offset-4">
+              cadrastre-se
+            </a>
+          </FieldDescription>
+        </Field>
+      </FieldGroup>
+    </form>
+  );
+}
